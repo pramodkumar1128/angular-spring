b9fe7bb26163da7282940cc48a97258b3c776bcc
Pramod Kumar A <pramodkumar1128@gmail.com>
2024-11-05 15:10:56 -0500

    commit

--- springboot-backend/app.py
+++ springboot-backend/app.py
@@ -1,10 +1,10 @@
 import os
 from random import randint
 
-for i in range(1, 365):
+for i in range(100, 200):
 
     for j in range(0, randint(1, 10)):
-        d = '100' + ' days ago'
+        d = str(i) + ' days ago'
         with open('file.txt', 'a') as file:
             file.write(d)
         os.system("git add .")
